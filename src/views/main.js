1aedd9ee7a79a58430f312c6fe32dd43de252015
Jonathan Berger <drskullster@pm.me>
2025-10-14 11:36:50 +0200

    Add flag to hide menu

--- src/views/main.js
+++ src/views/main.js
@@ -13,6 +13,10 @@ export default function mainView(state, emit) {
         yo = false;
     }
 
+    
+    const urlParams = new URLSearchParams(window.location.search);
+    const hideMenu = urlParams.get('hide-menu');
+
     return html`
       <body>
       <div id="hydra-ui">
@@ -23,29 +27,30 @@ export default function mainView(state, emit) {
 
       ${state.cache(Editor, 'editor').render(state, emit)}
 
-
-      <div id="tarifs">
-        <dl>
-          <dt>Eau plate</dt>
-          <dd>Gratuit</dd>
-        </dl>
-        <dl>
-          <dt>Coca / Coca zéro</dt>
-          <dd>2€</dd>
-        </dl>
-        <dl>
-          <dt>Pils 33cl</dt>
-          <dd>2,5€</dd>
-        </dl>
-        <dl>
-          <dt>Jupiler 0.0</dt>
-          <dd>2€</dd>
-        </dl>
-        <dl>
-          <dt>Delta IPA / Duvel</dt>
-          <dd>4.5€</dd>
-        </dl>
-      </div>
+      ${!hideMenu && (
+          <div id="tarifs">
+            <dl>
+              <dt>Eau plate</dt>
+              <dd>Gratuit</dd>
+            </dl>
+            <dl>
+              <dt>Coca / Coca zéro</dt>
+              <dd>2€</dd>
+            </dl>
+            <dl>
+              <dt>Pils 33cl</dt>
+              <dd>2,5€</dd>
+            </dl>
+            <dl>
+              <dt>Jupiler 0.0</dt>
+              <dd>2€</dd>
+            </dl>
+            <dl>
+              <dt>Delta IPA / Duvel</dt>
+              <dd>4.5€</dd>
+            </dl>
+          </div>
+      )}
 
       </body>
     `

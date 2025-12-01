671f515bfa610d6a81067864898978cf5655ec74
Jonathan Berger <drskullster@pm.me>
2025-12-01 14:40:15 +0100

    Up

--- src/views/main.js
+++ src/views/main.js
@@ -24,24 +24,20 @@ export default function mainView(state, emit) {
           <dd>Gratuit</dd>
         </dl>
         <dl>
-          <dt>Coca / Coca zéro</dt>
-          <dd>2€</dd>
+          <dt>Softs</dt>
+          <dd>2€ / 2.5€</dd>
         </dl>
         <dl>
           <dt>Jupiler / Jupiler 0.0</dt>
           <dd>2,5€</dd>
         </dl>
         <dl>
-          <dt>Delta IPA / Duvel Triple Hop</dt>
-          <dd>4.5€</dd>
-        </dl>
-<!--        <dl>-->
-<!--          <dt>Vin Blanc / Rouge</dt>-->
-<!--          <dd>4€</dd>-->
-<!--        </dl>-->
-        <dl>
           <dt>Maté-Maté</dt>
           <dd>4€</dd>
+        </dl>
+        <dl>
+          <dt>IPA / IPA 0.0</dt>
+          <dd>4.5€</dd>
         </dl>
       </div>
     `;
